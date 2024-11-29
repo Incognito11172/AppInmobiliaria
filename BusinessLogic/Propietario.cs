556081820b0a6bd32302f9a58466e51e15847424
JuinorG29 <Xnemix18@gmail.com>
2024-11-28 23:15:20 -0400

    Implementacion de la funcion añadir propietario.

--- BusinessLogic/Propietario.cs
+++ BusinessLogic/Propietario.cs
@@ -1,7 +1,40 @@
-﻿namespace BusinessLogic
+﻿using DataAccess;
+using Microsoft.Data.SqlClient;
+
+namespace BusinessLogic
 {
     public class Propietario
     {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
 
+        public void Add()
+        {
+            try
+            {
+                string query = "insert into Propietario (nombre, telefono) values (@nombre, @telefono)";
+
+                DA dbAccess = new DA();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@telefono", telefono);
+
+                dbAccess.ExecuteQuery(cmd);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
+ 
+    
+
+    
+
