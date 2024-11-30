6656020fdccb067ce2e0ae7401d6e68e4e3be363
nicolefdz07 <n.fernandez17@unapec.edu.do>
2024-11-29 20:51:29 -0400

    implementing Add function to the inmueble class

--- BusinessLogic/Inmueble.cs
+++ BusinessLogic/Inmueble.cs
@@ -8,5 +8,41 @@ namespace BusinessLogic
 {
     public class Inmueble
     {
+        public int REF { get; set; }    
+        public int? id_oficina { get; set; }    
+        public int? id_propietario { get; set; }
+        public float precio_venta { get; set; } 
+        public float precio_alquiler { get; set; }
+        public string? direccion { get; set; }
+        public float metros_cuadrados { get; set; }
+        public string? ruta_foto { get; set; }
+
+        public void Add()
+        {
+            try
+            {
+                string query = "insert into Inmueble (id_oficina, id_propietario, precio_venta, precio_alquiler, direccion, metros_cuadrados, ruta_foto) values (@id_oficina, @id_propietario, @precio_venta, @precio_alquiler, @direccion, @metros_cuadrados, @ruta_foto)";
+
+                DA dbAccess = new DA();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@id_oficina", id_oficina);
+                cmd.Parameters.AddWithValue("@id_propietario", id_propietario);
+                cmd.Parameters.AddWithValue("@precio_venta", precio_venta);
+                cmd.Parameters.AddWithValue("@precio_alquiler", precio_alquiler);
+                cmd.Parameters.AddWithValue("@direccion", direccion);
+                cmd.Parameters.AddWithValue("@metros_cuadrados", metros_cuadrados);
+                cmd.Parameters.AddWithValue("@ruta_foto", ruta_foto);
+
+                dbAccess.ExecuteQuery(cmd);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
