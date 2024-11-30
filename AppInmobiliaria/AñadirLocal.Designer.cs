45982ff8e7c8ace453d3c4e09ba9fbaf9a705906
leorivass <leorivas1805@gmail.com>
2024-11-30 13:47:35 -0400

    Added controls to register 'Vivienda' or 'Local' estate in the database.

--- AppInmobiliaria/AñadirLocal.Designer.cs
+++ AppInmobiliaria/AñadirLocal.Designer.cs
@@ -28,12 +28,332 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AñadirLocal";
+            panel1 = new Panel();
+            panel2 = new Panel();
+            materialCard2 = new MaterialSkin.Controls.MaterialCard();
+            materialCheckedListBox1 = new MaterialSkin.Controls.MaterialCheckedListBox();
+            materialCheckbox5 = new MaterialSkin.Controls.MaterialCheckbox();
+            materialCheckbox4 = new MaterialSkin.Controls.MaterialCheckbox();
+            materialCheckbox3 = new MaterialSkin.Controls.MaterialCheckbox();
+            materialCheckbox2 = new MaterialSkin.Controls.MaterialCheckbox();
+            materialCheckbox1 = new MaterialSkin.Controls.MaterialCheckbox();
+            materialTextBox22 = new MaterialSkin.Controls.MaterialTextBox2();
+            materialTextBox21 = new MaterialSkin.Controls.MaterialTextBox2();
+            nombrePropietario = new MaterialSkin.Controls.MaterialTextBox2();
+            label1 = new Label();
+            materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            materialCard2.SuspendLayout();
+            materialCheckedListBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.RoyalBlue;
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(574, 100);
+            panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.RoyalBlue;
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 490);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(574, 100);
+            panel2.TabIndex = 3;
+            // 
+            // materialCard2
+            // 
+            materialCard2.BackColor = Color.FromArgb(255, 255, 255);
+            materialCard2.Controls.Add(materialButton2);
+            materialCard2.Controls.Add(materialButton1);
+            materialCard2.Controls.Add(materialCheckedListBox1);
+            materialCard2.Controls.Add(materialTextBox22);
+            materialCard2.Controls.Add(materialTextBox21);
+            materialCard2.Controls.Add(nombrePropietario);
+            materialCard2.Controls.Add(label1);
+            materialCard2.Depth = 0;
+            materialCard2.Font = new Font("Segoe UI", 20F);
+            materialCard2.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            materialCard2.Location = new Point(14, 117);
+            materialCard2.Margin = new Padding(14);
+            materialCard2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCard2.Name = "materialCard2";
+            materialCard2.Padding = new Padding(14);
+            materialCard2.Size = new Size(544, 344);
+            materialCard2.TabIndex = 4;
+            // 
+            // materialCheckedListBox1
+            // 
+            materialCheckedListBox1.AutoScroll = true;
+            materialCheckedListBox1.BackColor = Color.FromArgb(255, 255, 255);
+            materialCheckedListBox1.Controls.Add(materialCheckbox5);
+            materialCheckedListBox1.Controls.Add(materialCheckbox4);
+            materialCheckedListBox1.Controls.Add(materialCheckbox3);
+            materialCheckedListBox1.Controls.Add(materialCheckbox2);
+            materialCheckedListBox1.Controls.Add(materialCheckbox1);
+            materialCheckedListBox1.Depth = 0;
+            materialCheckedListBox1.Location = new Point(323, 71);
+            materialCheckedListBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCheckedListBox1.Name = "materialCheckedListBox1";
+            materialCheckedListBox1.Size = new Size(169, 205);
+            materialCheckedListBox1.Striped = false;
+            materialCheckedListBox1.StripeDarkColor = Color.Empty;
+            materialCheckedListBox1.TabIndex = 4;
+            // 
+            // materialCheckbox5
+            // 
+            materialCheckbox5.AutoSize = true;
+            materialCheckbox5.Depth = 0;
+            materialCheckbox5.Location = new Point(16, 122);
+            materialCheckbox5.Margin = new Padding(0);
+            materialCheckbox5.MouseLocation = new Point(-1, -1);
+            materialCheckbox5.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCheckbox5.Name = "materialCheckbox5";
+            materialCheckbox5.ReadOnly = false;
+            materialCheckbox5.Ripple = true;
+            materialCheckbox5.Size = new Size(61, 37);
+            materialCheckbox5.TabIndex = 4;
+            materialCheckbox5.Text = "Luz";
+            materialCheckbox5.UseVisualStyleBackColor = true;
+            // 
+            // materialCheckbox4
+            // 
+            materialCheckbox4.AutoSize = true;
+            materialCheckbox4.Depth = 0;
+            materialCheckbox4.Location = new Point(16, 11);
+            materialCheckbox4.Margin = new Padding(0);
+            materialCheckbox4.MouseLocation = new Point(-1, -1);
+            materialCheckbox4.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCheckbox4.Name = "materialCheckbox4";
+            materialCheckbox4.ReadOnly = false;
+            materialCheckbox4.Ripple = true;
+            materialCheckbox4.Size = new Size(141, 37);
+            materialCheckbox4.TabIndex = 3;
+            materialCheckbox4.Text = "Acondicionado";
+            materialCheckbox4.UseVisualStyleBackColor = true;
+            // 
+            // materialCheckbox3
+            // 
+            materialCheckbox3.AutoSize = true;
+            materialCheckbox3.Depth = 0;
+            materialCheckbox3.Location = new Point(16, 85);
+            materialCheckbox3.Margin = new Padding(0);
+            materialCheckbox3.MouseLocation = new Point(-1, -1);
+            materialCheckbox3.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCheckbox3.Name = "materialCheckbox3";
+            materialCheckbox3.ReadOnly = false;
+            materialCheckbox3.Ripple = true;
+            materialCheckbox3.Size = new Size(72, 37);
+            materialCheckbox3.TabIndex = 2;
+            materialCheckbox3.Text = "Agua";
+            materialCheckbox3.UseVisualStyleBackColor = true;
+            // 
+            // materialCheckbox2
+            // 
+            materialCheckbox2.AutoSize = true;
+            materialCheckbox2.Depth = 0;
+            materialCheckbox2.Location = new Point(16, 48);
+            materialCheckbox2.Margin = new Padding(0);
+            materialCheckbox2.MouseLocation = new Point(-1, -1);
+            materialCheckbox2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCheckbox2.Name = "materialCheckbox2";
+            materialCheckbox2.ReadOnly = false;
+            materialCheckbox2.Ripple = true;
+            materialCheckbox2.Size = new Size(72, 37);
+            materialCheckbox2.TabIndex = 1;
+            materialCheckbox2.Text = "Vado";
+            materialCheckbox2.UseVisualStyleBackColor = true;
+            // 
+            // materialCheckbox1
+            // 
+            materialCheckbox1.AutoSize = true;
+            materialCheckbox1.Depth = 0;
+            materialCheckbox1.Location = new Point(16, 159);
+            materialCheckbox1.Margin = new Padding(0);
+            materialCheckbox1.MouseLocation = new Point(-1, -1);
+            materialCheckbox1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCheckbox1.Name = "materialCheckbox1";
+            materialCheckbox1.ReadOnly = false;
+            materialCheckbox1.Ripple = true;
+            materialCheckbox1.Size = new Size(92, 37);
+            materialCheckbox1.TabIndex = 0;
+            materialCheckbox1.Text = "Diáfano";
+            materialCheckbox1.UseVisualStyleBackColor = true;
+            // 
+            // materialTextBox22
+            // 
+            materialTextBox22.AnimateReadOnly = false;
+            materialTextBox22.BackgroundImageLayout = ImageLayout.None;
+            materialTextBox22.CharacterCasing = CharacterCasing.Normal;
+            materialTextBox22.Depth = 0;
+            materialTextBox22.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialTextBox22.HideSelection = true;
+            materialTextBox22.Hint = "Número de puertas";
+            materialTextBox22.LeadingIcon = null;
+            materialTextBox22.Location = new Point(55, 145);
+            materialTextBox22.MaxLength = 32767;
+            materialTextBox22.MouseState = MaterialSkin.MouseState.OUT;
+            materialTextBox22.Name = "materialTextBox22";
+            materialTextBox22.PasswordChar = '\0';
+            materialTextBox22.PrefixSuffixText = null;
+            materialTextBox22.ReadOnly = false;
+            materialTextBox22.RightToLeft = RightToLeft.No;
+            materialTextBox22.SelectedText = "";
+            materialTextBox22.SelectionLength = 0;
+            materialTextBox22.SelectionStart = 0;
+            materialTextBox22.ShortcutsEnabled = true;
+            materialTextBox22.Size = new Size(251, 48);
+            materialTextBox22.TabIndex = 5;
+            materialTextBox22.TabStop = false;
+            materialTextBox22.TextAlign = HorizontalAlignment.Left;
+            materialTextBox22.TrailingIcon = null;
+            materialTextBox22.UseSystemPasswordChar = false;
+            // 
+            // materialTextBox21
+            // 
+            materialTextBox21.AnimateReadOnly = false;
+            materialTextBox21.BackgroundImageLayout = ImageLayout.None;
+            materialTextBox21.CharacterCasing = CharacterCasing.Normal;
+            materialTextBox21.Depth = 0;
+            materialTextBox21.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialTextBox21.HideSelection = true;
+            materialTextBox21.Hint = "Altura del altillo";
+            materialTextBox21.LeadingIcon = null;
+            materialTextBox21.Location = new Point(55, 219);
+            materialTextBox21.MaxLength = 32767;
+            materialTextBox21.MouseState = MaterialSkin.MouseState.OUT;
+            materialTextBox21.Name = "materialTextBox21";
+            materialTextBox21.PasswordChar = '\0';
+            materialTextBox21.PrefixSuffixText = null;
+            materialTextBox21.ReadOnly = false;
+            materialTextBox21.RightToLeft = RightToLeft.No;
+            materialTextBox21.SelectedText = "";
+            materialTextBox21.SelectionLength = 0;
+            materialTextBox21.SelectionStart = 0;
+            materialTextBox21.ShortcutsEnabled = true;
+            materialTextBox21.Size = new Size(251, 48);
+            materialTextBox21.TabIndex = 3;
+            materialTextBox21.TabStop = false;
+            materialTextBox21.TextAlign = HorizontalAlignment.Left;
+            materialTextBox21.TrailingIcon = null;
+            materialTextBox21.UseSystemPasswordChar = false;
+            // 
+            // nombrePropietario
+            // 
+            nombrePropietario.AnimateReadOnly = false;
+            nombrePropietario.BackgroundImageLayout = ImageLayout.None;
+            nombrePropietario.CharacterCasing = CharacterCasing.Normal;
+            nombrePropietario.Depth = 0;
+            nombrePropietario.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            nombrePropietario.HideSelection = true;
+            nombrePropietario.Hint = "Zona de ubicación";
+            nombrePropietario.LeadingIcon = null;
+            nombrePropietario.Location = new Point(55, 82);
+            nombrePropietario.MaxLength = 32767;
+            nombrePropietario.MouseState = MaterialSkin.MouseState.OUT;
+            nombrePropietario.Name = "nombrePropietario";
+            nombrePropietario.PasswordChar = '\0';
+            nombrePropietario.PrefixSuffixText = null;
+            nombrePropietario.ReadOnly = false;
+            nombrePropietario.RightToLeft = RightToLeft.No;
+            nombrePropietario.SelectedText = "";
+            nombrePropietario.SelectionLength = 0;
+            nombrePropietario.SelectionStart = 0;
+            nombrePropietario.ShortcutsEnabled = true;
+            nombrePropietario.Size = new Size(251, 48);
+            nombrePropietario.TabIndex = 2;
+            nombrePropietario.TabStop = false;
+            nombrePropietario.TextAlign = HorizontalAlignment.Left;
+            nombrePropietario.TrailingIcon = null;
+            nombrePropietario.UseSystemPasswordChar = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI Semibold", 20F);
+            label1.Location = new Point(190, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(196, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Datos del local";
+            // 
+            // materialButton1
+            // 
+            materialButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton1.Depth = 0;
+            materialButton1.HighEmphasis = true;
+            materialButton1.Icon = null;
+            materialButton1.Location = new Point(158, 288);
+            materialButton1.Margin = new Padding(4, 6, 4, 6);
+            materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton1.Name = "materialButton1";
+            materialButton1.NoAccentTextColor = Color.Empty;
+            materialButton1.Size = new Size(104, 36);
+            materialButton1.TabIndex = 8;
+            materialButton1.Text = "Continuar";
+            materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton1.UseAccentColor = false;
+            materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialButton2
+            // 
+            materialButton2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton2.Depth = 0;
+            materialButton2.HighEmphasis = true;
+            materialButton2.Icon = null;
+            materialButton2.Location = new Point(290, 288);
+            materialButton2.Margin = new Padding(4, 6, 4, 6);
+            materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton2.Name = "materialButton2";
+            materialButton2.NoAccentTextColor = Color.Empty;
+            materialButton2.Size = new Size(96, 36);
+            materialButton2.TabIndex = 9;
+            materialButton2.Text = "Cancelar";
+            materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton2.UseAccentColor = false;
+            materialButton2.UseVisualStyleBackColor = true;
+            // 
+            // AñadirLocal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(574, 590);
+            Controls.Add(materialCard2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "AñadirLocal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AñadirLocal";
+            materialCard2.ResumeLayout(false);
+            materialCard2.PerformLayout();
+            materialCheckedListBox1.ResumeLayout(false);
+            materialCheckedListBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private MaterialSkin.Controls.MaterialCard materialCard2;
+        private MaterialSkin.Controls.MaterialCheckedListBox materialCheckedListBox1;
+        private MaterialSkin.Controls.MaterialCheckbox materialCheckbox4;
+        private MaterialSkin.Controls.MaterialCheckbox materialCheckbox3;
+        private MaterialSkin.Controls.MaterialCheckbox materialCheckbox2;
+        private MaterialSkin.Controls.MaterialCheckbox materialCheckbox1;
+        private MaterialSkin.Controls.MaterialTextBox2 materialTextBox22;
+        private MaterialSkin.Controls.MaterialTextBox2 materialTextBox21;
+        private MaterialSkin.Controls.MaterialTextBox2 nombrePropietario;
+        private Label label1;
+        private MaterialSkin.Controls.MaterialCheckbox materialCheckbox5;
+        private MaterialSkin.Controls.MaterialButton materialButton2;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
     }
 }
